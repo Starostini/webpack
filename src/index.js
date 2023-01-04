2acfb7fe8a9209b9ec258d665db90c480805ae14
Ilya Starostin <starostini@hotmail.com>
2023-01-04 14:20:13 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import './index.html';
+import './index.scss';
+import imageexample from './img/INSTALL_EXTENSION_STEP_2_IMAGE.png'
+import { mathem, sum } from './modules/calc';
+
+const img = new Image();
+img.src = imageexample;
+console.log(mathem(2, 4));
+console.log(sum(6, 2));
---
+++ src/modules/calc.js
@@ -0,0 +1,2 @@
+export const mathem = (a, b) => a * b;
+export const sum = async (a, b) => a + b;
